4adbd08a373c27a721334098549bb5df383f8cb9
Harsh Harit <harsh.h@cloudharbortechnologies.com>
2026-05-12 03:20:33 +0530

    added scripts and next config for deployments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "api.dicebear.com" },
+    ],
+  },
 };
 
 export default nextConfig;
